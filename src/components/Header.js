92e49761264631f2ed5da783fe427011b8b0bd44
Varshil <>
2021-09-27 16:35:38 +0530

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const Header = () => {
+  return (
+    <h2 style={{ color: '#212121', fontSize: '30px', textAlign: "center" }}><u>React Expense Tracker</u></h2>
+  );
+};
