de68031f2ca9fac47b592bd69c729f7a4a83b813
Varshil <>
2021-09-27 17:28:54 +0530

    Updates

---
+++ precache-manifest.ec585c7a49cb3607a350a83308d931f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b857504edeaa46f1dff0fa81747590bb",
+    "url": "/ExpenseTracker/index.html"
+  },
+  {
+    "revision": "4383c3158dd3e7256c98",
+    "url": "/ExpenseTracker/static/css/main.d70ed8e8.chunk.css"
+  },
+  {
+    "revision": "fc14657f00f81185107f",
+    "url": "/ExpenseTracker/static/js/2.fc7a94a2.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/ExpenseTracker/static/js/2.fc7a94a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4383c3158dd3e7256c98",
+    "url": "/ExpenseTracker/static/js/main.10ec050b.chunk.js"
+  },
+  {
+    "revision": "6d34a13ade7a9e2d134d",
+    "url": "/ExpenseTracker/static/js/runtime-main.50ffb5a9.js"
+  }
+]);
